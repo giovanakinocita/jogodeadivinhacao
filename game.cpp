23a40753579aa74a5a6dc4d4485b0aadbbe27e47
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-06-25 00:15:23 -0300

    Lidando com variáveis

--- game.cpp
+++ game.cpp
@@ -6,4 +6,7 @@ int main()
     cout << "***************************************" << endl;
     cout << "* Boas vindas ao Jogo da Adivinhação! *" << endl;
     cout << "***************************************" << endl;
+
+    int numero_secreto = 55;
+    cout << "O número secreto é " << numero_secreto << ". Não conte a ninguém!" << endl;
 }
