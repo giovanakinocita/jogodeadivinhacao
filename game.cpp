56773232d0ba0739c7d1316ef13b386b569e5410
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-06-24 23:25:15 -0300

    Primeiro Commit

---
+++ game.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "***************************************" << endl;
+    cout << "* Boas vindas ao Jogo da Adivinhação! *" << endl;
+    cout << "***************************************" << endl;
+}
